45660c93eb6feac5a09487952bf36f63897d82e1
cem <cemkaya.boun@gmail.com>
2021-06-02 14:41:17 +0300

    Deleted file for merge request.

--- person/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Person
-
-class addPersonForm(forms.ModelForm):
-    class Meta:
-        model = Person
-        fields = ["title","firstname","lastname","location","email","age","phone","imageUrl"]
