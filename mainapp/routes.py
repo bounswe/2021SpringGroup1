91b7ebe81104f66d3767c6dfee193ceecb2ce763
cem <cemkaya.boun@gmail.com>
2021-06-02 20:58:13 +0300

    Added create community html page.

--- mainapp/routes.py
+++ mainapp/routes.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("mainPage", views.mainPage, name="mainPage"),
     path("getAllCommuntiesOfUser/", views.mainPage, name="mainPage"),
     path("createCommunity_ui/",views.createCommunity_ui,name="createCommunity_ui"),
-    path("viewCommunity_ui/",views.viewCommunity_ui,name="viewCommunity_ui")
+    path("viewCommunity_ui/",views.viewCommunity_ui,name="viewCommunity_ui"),
+    path("getSuggestions/",views.getSuggestions,name="getSuggestions")
 ]
