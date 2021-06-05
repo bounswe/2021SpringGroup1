da7b6bd221217da81887a4c5623351aebcbc3eb1
Berke Argın <berke.argin98@gmail.com>
2021-06-05 14:26:43 +0300

    First commit of  mainapp-berke
    This commits holds a partially working version of mainapp. There are a lot of TODOs so  feel free to inspect.

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from person.models import Person
-
+import json
 # Create your models here.
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, verbose_name="Topluluk Adı")
+    description = models.TextField(max_length=max, verbose_name='Description')
     moderator = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='moderator') #A community has only one moderator.
     numUsers = models.IntegerField(verbose_name = "Kullanıcı Sayısı")
     numPosts = models.IntegerField(verbose_name = "Post Sayısı")
@@ -13,11 +14,11 @@ class Community(models.Model):
     joinedUsers=models.ManyToManyField(Person,related_name='joinedCommunities')
     #TODO:Update this string.
 
-    def __str__(self):
+    def __str__(self)-> str:
         data = {
             "id": self.id,
             "name": self.name,
-            "moderator_name": self.moderator.name,
+            "moderator_name": self.moderator.firstname,
             "numUsers": self.numUsers,
             "numPosts": self.numPosts,
             "isPrivate": self.isPrivate
@@ -28,15 +29,19 @@ class PostTemplate(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='Id')
     name=models.CharField(max_length=50, verbose_name='Name')
     description = models.TextField(max_length=max, verbose_name='Description')
-    data_field_temps=models.JSONField(max_length=max, verbose_name='DataFieldTemps',default='{}')
     community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='post_templates') #A post can be in only one community.
     
     def __str__(self) -> str:
+        dataFields=self.dataFieldTemplates.all()
+        dataFieldDict={}
+        for d in dataFields:
+            dataFieldDict[d.id]=d._str_()
+        
         data = {
             "id": self.id,
             "name": self.name,
             "description": self.description,
-            "data_field_temps": self.data_field_temps
+            "data_field_temps": json.dumps(self.dataFieldDict)
         }
         return data
 
@@ -47,35 +52,31 @@ class Post(models.Model):
     description = models.TextField(max_length=max, verbose_name='Description')
     createdDate = models.DateTimeField(
         auto_now_add=True, verbose_name="Created Date")
-    dataFields=models.JSONField(max_length=max, verbose_name='Data',default='{}')
     postTemplate=models.ForeignKey(PostTemplate, on_delete=models.CASCADE)
     community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='posts') #A post can be in only one community.
     
-    #def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-    #    dataFieldDict = json.loads(self.dataFields)
-    #    if not dataFieldDict:
-    #        self.dataFields = '{}'
-    #    else:
-    #        self.dataFields = json.dumps(data_dict)
-    #    return super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
-    
     def __str__(self) -> str:
+        dataFields=self.dataFields.all()
+        dataFieldDict={}
+        for d in dataFields:
+            dataFieldDict[d.id]=d._str_()
+        
         data = {
             "id": self.id,
             "posterid": self.posterid,
             "title": self.title,
             "description": self.description,
             "date": self.createdDate,
-            "dataFields":self.dataFields
+            "dataFields":json.dumps(self.dataFieldDict)
         }
         return data
 
 class DataField(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='Id')
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='dataFields')
     name=models.CharField(max_length=50, verbose_name='Name')
     type=models.CharField(max_length=50, verbose_name='Type')
-    content=models.JSONField(max_length=max, verbose_name='Data',default={})
+    content=models.JSONField(max_length=max, verbose_name='Data')
     def __str__(self) -> str:
         data = {
             "id": self.id,
@@ -90,8 +91,8 @@ class DataFieldTemp(models.Model):
     id = models.AutoField(primary_key=True, verbose_name='Id')
     name=models.CharField(max_length=50, verbose_name='Name')
     type=models.CharField(max_length=50, verbose_name='Type')
-    form_content=models.JSONField(max_length=max, verbose_name='Data',default={})
-    postTemplate=models.ForeignKey(PostTemplate, on_delete=models.CASCADE)
+    form_content=models.JSONField(max_length=max, verbose_name='Data')
+    postTemplate=models.ForeignKey(PostTemplate, on_delete=models.CASCADE, related_name='dataFieldTemplates')
     def __str__(self) -> str:
         data = {
             "id": self.id,
--- mainapp/routes.py
+++ mainapp/routes.py
@@ -6,10 +6,14 @@ app_name = "mainapp"
 
 urlpatterns = [
     path("mainPage", views.mainPage, name="mainPage"),
-    path("login_ui/", views.login_ui, name="login_ui"),
+    #path("login_ui/", views.login_ui, name="login_ui"),
     path("createCommunity_ui/",views.createCommunity_ui,name="createCommunity_ui"),
     path("viewCommunity_ui/",views.viewCommunity_ui,name="viewCommunity_ui"),
-    //TODO:remove non-ui paths.
+    #TODO:remove non-ui paths.
     path("getSuggestions/",views.getSuggestions,name="getSuggestions"),
-    path("getAllCommuntiesOfUser/", views.mainPage, name="mainPage"),
+    path("getAllCommunitiesOfUser/", views.getAllCommunitiesOfUser, name="getAllCommunitiesOfUser"),
+    path("createCommunity/",views.createCommunity, name="createCommunity"),
+    path("getAllPostsOfCommunity/", views.getAllPostsOfCommunity, name="getAllPostsOfCommunity"),
+    path("createPostTemplate_ui/",views.createPostTemplate_ui,name="createPostTemplate_ui"),
+    path("createPostTemplate/",views.createPostTemplate,name="createPostTemplate")
 ]
