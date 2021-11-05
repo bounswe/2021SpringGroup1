0e6504cab3eee9b04f7c80a12b20da40aa0f6c36
emrahdgn <doganemrah7@gmail.com>
2021-11-05 22:46:29 +0300

    Register Function Prototype
    Register Function Prototype implementation

--- cmpe451/protopost/register.py
+++ cmpe451/protopost/register.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def try_register(req):
+	if req.method == 'POST':
+		user=User()
+		if not "user_name" in req.POST:
+			return HttpResponse("Fail, no username.")
+		else:
+			user.user_name=req.POST["user_name"]
+		if not "password" in req.POST:
+			return HttpResponse("Fail, no password.")
+		else:
+			user.password=req.POST["password"]
+			user.save()
+		return HttpResponse("Success")
 
 
-def register(request):
 
-
-
-	pass
--- cmpe451/protopost/urls.py
+++ cmpe451/protopost/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
+from . import register
 
 urlpatterns = [
 	path("", views.home, name="home_pages"),
+	path("register", register.try_register, name= "register_func" )
 ]
