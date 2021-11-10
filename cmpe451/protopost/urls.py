0812fabc20ff51ab474f831ccb31a9a751eafdfd
emrahdgn <doganemrah7@gmail.com>
2021-11-10 12:11:53 +0300

    Creating Community
    implementing Creating Community function

--- cmpe451/protopost/urls.py
+++ cmpe451/protopost/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
-from . import register
+from . import register, home
 
 urlpatterns = [
 	path("", views.home, name="home_pages"),
 	path("try_register", register.try_register, name= "try_register" ),
 	path("try_login", register.try_login, name= "try_login" ),
 	path("test_auth",register.test_auth, name="test_auth"),
-	path("try_logout",register.try_logout, name="try_logout")
+	path("try_logout",register.try_logout, name="try_logout"),
+	path("try_community", home.try_create_community, name="try_create_community"),
 ]
