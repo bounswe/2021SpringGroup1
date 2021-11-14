5088c57dfafe6c32e8590eff9bf9a17006cd0f9f
gktpmuhammed <goktepemuhammed41@hotmail.com>
2021-11-14 23:14:22 +0300

    Create Community page improved.

--- Web/src/screens/createPostPage.js
+++ Web/src/screens/createPostPage.js
@@ -8,9 +8,8 @@ import { Card, Col, Container, Dropdown, DropdownButton, Form, FormGroup, FormLa
 import { useHistory } from "react-router-dom";
 
 
-
 function CreatePostPage(props) {
-    const history= useHistory();
+    const history = useHistory();
 
     const exampleData = {
         data: [{ "id": "15", "name": "name1", "dataType": "Text" }, { "id": "16", "name": "name2", "dataType": "Text" }
@@ -22,7 +21,6 @@ function CreatePostPage(props) {
         console.log(files);
     }
     return (
-
         <>
             <div>
                 <SideBar />
@@ -39,10 +37,10 @@ function CreatePostPage(props) {
                             <div >
                                 <Row>
                                     <Col>
-                                        <FormLabel style={{color: "black"}} > {field["name"]} </FormLabel>
+                                        <FormLabel style={{ color: "black" }} > {field["name"]} </FormLabel>
                                     </Col>
-                                    {field["dataType"] === "Text" && <Col xs={8}><input type="text" name="textField"></input></Col>} 
-                                    {field["dataType"] === "Image" &&<Col xs={8}><input type="file" name="file" accept="image/*" onChange={(e) => onChangeFile(e)}></input>
+                                    {field["dataType"] === "Text" && <Col xs={8}><input type="text" name="textField"></input></Col>}
+                                    {field["dataType"] === "Image" && <Col xs={8}><input type="file" name="file" accept="image/*" onChange={(e) => onChangeFile(e)}></input>
                                     </Col>}
                                 </Row>
                             </div>
@@ -51,53 +49,53 @@ function CreatePostPage(props) {
                 </Card.Body>
             </Card>
             <div>
-            <Card style={{ width: '15rem', margin: 'auto',position:"absolute",right: "5px",top: "5px" }}>
-            <Card.Img variant="top" src="https://i4.hurimg.com/i/hurriyet/75/1110x740/5b8e6d967152d827603dd434.jpg" />
-            <Card.Body>
-            <Card.Title style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>Community Name</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-            
+                <Card style={{ width: '15rem', margin: 'auto', position: "absolute", right: "5px", top: "5px" }}>
+                    <Card.Img variant="top" src="https://i4.hurimg.com/i/hurriyet/75/1110x740/5b8e6d967152d827603dd434.jpg" />
+                    
+                    <Card.Body>
+                        <Card.Title style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>Community Name</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                    </Card.Body>
 
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-            <ListGroupItem style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-        <Button variant="success">Subscribe</Button>{' '}
-        </ListGroupItem>
-        <ListGroupItem style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-        <Button onClick={()=>history.push('/community/'+props.match.params.id)} variant="success">Feed Page</Button>{' '}
-        </ListGroupItem>
-        <ListGroupItem style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>    
-        <Button variant="success">Create Post</Button>{' '}
-        </ListGroupItem>
-        <ListGroupItem style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-        <Button onClick={()=>history.push('/community/createPostTemplate/'+props.match.params.id)} variant="success">Create Post Template</Button>{' '}
-        </ListGroupItem>
-        </ListGroup>
-            </Card>
-        </div>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Button variant="success">Subscribe</Button>{' '}
+                        </ListGroupItem>
+                        <ListGroupItem style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Button onClick={() => history.push('/community/' + props.match.params.id)} variant="success">Feed Page</Button>{' '}
+                        </ListGroupItem>
+                        <ListGroupItem style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Button variant="success">Create Post</Button>{' '}
+                        </ListGroupItem>
+                        <ListGroupItem style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Button onClick={() => history.push('/community/createPostTemplate/' + props.match.params.id)} variant="success">Create Post Template</Button>{' '}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Card>
+            </div>
         </>
     );
 }
--- Web/src/screens/home.js
+++ Web/src/screens/home.js
@@ -1,7 +1,7 @@
 //React Library
-import React, {useState,useEffect,} from 'react';
-import {Link, Redirect} from 'react-router-dom';
-import {urls} from 'DATABASE';
+import React, { useState, useEffect, } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { urls } from 'DATABASE';
 import 'assets/css/home.css';
 
 import SignUpForm from '../components/SignUpForm';
@@ -9,14 +9,14 @@ import LoginForm from '../components/LoginForm';
 
 const styles = {
   anasayfa: {
-    position:"absolute",
-    top:'50%',
+    position: "absolute",
+    top: '50%',
     left: '50%',
     transform: "translate(-50%, -50%)"
   },
   links: {
-    color:"red",
-    fontSize:55,
+    color: "red",
+    fontSize: 55,
   }
 }
 
@@ -27,17 +27,14 @@ const Home = (props) => {
   return (
 
     <>
-    <div>
-    <div className='form-container'>
-        <span className='close-btn'>×</span>
-          <SignUpForm/>
-          <LoginForm/>
+      <div>
+        <div className='form-container'>
+          <span className='close-btn'>×</span>
+          <SignUpForm />
+          <LoginForm />
+        </div>
       </div>
-      {/* <Link to={urls.bootstrap} style={styles.links}>Bootstrap</Link>
-      <br />
-      <Link to={urls.profile} style={styles.links}>Profile</Link> */}
-    </div>
-      </>
+    </>
   );
 }
 
